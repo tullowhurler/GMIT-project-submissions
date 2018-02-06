8672814a94db56cfd0c86579c03dd0bb80bda15d
tullowhurler <35730844+tullowhurler@users.noreply.github.com>
2018-02-06 22:02:54 +0000

    Update Collatz.py

--- Collatz.py
+++ Collatz.py
@@ -1,5 +1,5 @@
 # Brian Condon 06/02/18
-# Collatz Conjecture
+# Collatz Conjecture without integer prompt
 
 n = 17 
 
@@ -12,4 +12,35 @@ while n == 17:
     print (n) 
     # first bit of code without the integer prompt, sequence ran 17,52,26,13,40,20,10,5,16,8,4,2,1
     
+# Part 2 with integer prompt 
+# Brian Condon 06/02/18
+# Collatz Conjecture
+
+n =  int(input("enter an integer:"))
+
+while n!= 1:
+  if n % 2 == 0:
+    n = n / 2  
+    print(n)
+  elif n % 2 >= 0:
+    n = n * 3 + 1
+    print (n) 
+    
+    C:\Users\Tullowhurler\Desktop\Programming and Scripting\Training> python collatz.py
+enter an integer: 22
+11.0
+34.0
+17.0
+52.0
+26.0
+13.0
+40.0
+20.0
+10.0
+5.0
+16.0
+8.0
+4.0
+2.0
+1.0
     
