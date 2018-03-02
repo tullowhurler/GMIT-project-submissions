656dfd9876b132f5cc48b75534e30e6754f58dfa
tullowhurler <G00364760@gmit.ie>
2018-03-02 18:13:00 +0000

    formatting practice

---
+++ formatting.py
@@ -0,0 +1,16 @@
+t = newTable
+("MyTable",5,3) # Create a new table with 5 rows and 3 columns
+print(t)
+
+
+
+
+
+
+
+
+
+
+
+
+
--- irisformat.py
+++ irisformat.py
@@ -1,2 +1,13 @@
 #Brian Condon
 # 28/02/18 Week 5 formatting exercise
+
+with open("data/iris.csv") as data_file:
+
+    for line in data_file:
+
+        line = line.replace(',', ' ')
+        line = line.rstrip()
+        print(line[:11], line[12:16].strip())
+      
+
+        
