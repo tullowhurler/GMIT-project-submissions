15d9da79f45761ced9d9491aedb3fe727464a7f8
tullowhurler <G00364760@gmit.ie>
2018-03-12 12:14:20 +0000

    2 solutions

---
+++ factorial.py
@@ -0,0 +1,27 @@
+# Brian Condon
+# 12/3/18
+# Exercise 6, Factorials 
+
+num = 5
+factorial = 1
+for i in range(1, num + 1):
+    factorial = factorial * i
+print("The factorial of" , num, "is", factorial)
+
+num1 = 7
+factorial = 1
+for i in range(1, num1 + 1):
+    factorial = factorial * i
+print("The factorial of" , num1, "is", factorial)
+
+num2 = 10
+factorial = 1
+for i in range(1, num2 + 1):
+    factorial = factorial * i
+print("The factorial of" , num2, "is", factorial)
+
+# I also found an even easier solution to the problem but was not sure if this was an adequate answer as there is very little code.
+import math
+print("The factorial of the number 5 is", math.factorial(5))
+print("The factorial of the number 7 is", math.factorial(7))
+print("The factorial of the number 10 is", math.factorial(10))
