1053e629a3043de59e8db723d60cc9775b84a5b5
tullowhurler <G00364760@gmit.ie>
2018-03-16 20:18:25 +0000

    fianl solution

--- skype2.py
+++ skype2.py
@@ -2,13 +2,13 @@
 #16/3/18 Ian's Solution
 
 def ispalindrome(s): # s is the string
-  ans = True 
+  ans = True # thats what will print out
   
-  for i in range(len(s)):
+  for i in range(len(s)): #  loops through s which we put down in print
       if s[i] != s[len(s) - 1 -i]: # len s of radar is = 5 as there is 5 digits, we want to get to 0-4 so have to -1, i starts at 0 and  
-          ans = False 
+          ans = False # if i is not = i returns false
 
-  return ans
+  return ans    # have to have return in the function
 
-print(ispalindrome("radar"))
-print(ispalindrome("radars"))
+print(ispalindrome("eye"))
+print(ispalindrome("eyes"))
