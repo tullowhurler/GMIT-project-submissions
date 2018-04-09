b6de7c3a86aac52184c9f1d9994ae4d4d596aede
tullowhurler <G00364760@gmit.ie>
2018-04-09 21:16:41 +0100

    new csv file, not sure if right

---
+++ oldgeyser.py
@@ -0,0 +1,14 @@
+#Old Geyser 
+#Brian Condon 9/4/18 Data Analysis Practice
+
+#Calculate the mean of each column
+import numpy
+
+#Read data file into array
+data = numpy.genfromtxt('data/oldgeyser.csv', delimiter=',')
+
+firstcol = data[:,0]
+meanfirstcol = numpy.mean(data[:,0])
+
+print("Average of first column is",meanfirstcol)
+
